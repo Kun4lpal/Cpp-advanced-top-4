73ba37fdf0df0816505237ce228926e8a54599e4
Kunal Paliwal <kunal_199219@yahoo.in>
2017-01-31 13:24:49 -0500

    updated on 1/31/2017(cpp)
    My basic implementations of some advanced DS with test cases.

---
+++ all Hw/Tree implementation and tests/BSTNode.h
@@ -0,0 +1,25 @@
+//
+//  BSTNode.h
+//  BinarySearchTree
+//
+//  Created by Kunal Paliwal on 11/13/16.
+//  Copyright © 2016 Kunal Paliwal BST. All rights reserved.
+//
+
+#ifndef BSTNode_h
+#define BSTNode_h
+
+
+
+class BSTNode
+{
+public:
+    BSTNode* left; // left pointer
+    BSTNode* right; // right pointer
+    int key; // nodes data -- key
+    int value; // nodes data -- value
+};
+
+
+
+#endif /* BSTNode_h */
